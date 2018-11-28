a633f1505bccfd1c859de20a682185d287c34ab4
FeiWu <fei1130@gmail.com>
2018-11-28 13:52:21 -0500

    Gernelized data path

--- LanguageModel/word_language_model/MonoGen.sh
+++ LanguageModel/word_language_model/MonoGen.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DataDir="/home/fei/Documents/JHU/18Fall/MT/FinalProject/Beta-VAE-in-Machine-Translation/LanguageModel/afrikaans"
+DataDir="afrikaans"
 echo "Data stored at:" $DataDir
 python3 main.py --data $DataDir
 python3 generate.py --data $DataDir
